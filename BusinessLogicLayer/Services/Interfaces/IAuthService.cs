0b36e0c9a94b1dfa4792905230818d86461a4a6d
Rusu Vlad <vlad.rusu@isa.utm.md>
2025-09-08 23:03:29 +0300

    Add base JWT authentication system without db
    Created an endpoint for authentication based on JWT tokens but lacking database integration so far. DB interaction requires another DI. Current one has hard coded values

---
+++ BusinessLogicLayer/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.Interfaces
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; }
+    }
+
+    public interface IAuthService
+    {
+        Task<AuthResponse> LoginAsync(string username, string password);
+    }
+}
