5603f78daa71c41d3a667401adb429a54e2b0651
Rusu Vlad <vlad.rusu@isa.utm.md>
2025-09-09 17:24:22 +0300

    Add base and user repositories and interfaces and dbcontext

--- DataAccessLayer/DatabaseContext.cs
+++ DataAccessLayer/DatabaseContext.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataAccessLayer.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer
 {
-    public partial class DatabaseContext() : DbContext()
+    public partial class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
     {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserSession> UserSessions { get; set; }
     }
 }
