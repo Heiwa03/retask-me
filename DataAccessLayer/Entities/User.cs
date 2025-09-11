0711ca69aea0bbeb3841e1b4e1ef512ad78269a8
Rusu Vlad <102832029+Heiwa03@users.noreply.github.com>
2025-09-11 16:20:03 +0300

    Add Seeder class for initial db testing and add gitignore (#4)
    * Created DAL and BaseId Entity
    
    * Create User and UserSession entities
    
    spizidili with modifications
    
    * Add IBaserepository interface
    
    * Add base and user repositories and interfaces and dbcontext
    
    * Create TestDataSeeder for db testing.
    
    * Add gitignore file
    
    * Nvm now gitignore should be added
    
    * Now surely gitignore should be working

--- DataAccessLayer/Entities/User.cs
+++ DataAccessLayer/Entities/User.cs
@@ -9,5 +9,5 @@ public class User : BaseId
 {
     [StringLength(255)] public required string Username { get; set; }
     [StringLength(255)] public required string Password { get; set; }
-    public required DateTime RegistrationDate { get; set; }
+    //public required DateTime RegistrationDate { get; set; }
 }
