2e2be420323659a055a162bcb19e0663c0118b06
BagrinD <bagrin.danu@gmail.com>
2025-09-11 09:10:12 +0300

    Some improvment in RegisterService

--- BusinessLogicLayer/DTOs/RegisterDTO.cs
+++ BusinessLogicLayer/DTOs/RegisterDTO.cs
@@ -1,6 +1,5 @@
 
 
-
 using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLogicLayer.DTOs{
@@ -11,12 +10,10 @@ namespace BusinessLogicLayer.DTOs{
         [MaxLength(100, ErrorMessage = "username is long asf")]
         public string Login { get; set; } = null!;
 
-
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password is shorter than...")]
         [MaxLength(20, ErrorMessage = "Password is huge bro")]
         public string Password { get; set; } = null!;
-
 
         [Required(ErrorMessage = "Rep password is required")]
         [MinLength(8, ErrorMessage = "Rep password is shorter than...")]
