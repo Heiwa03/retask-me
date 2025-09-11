f5c5cd8e5257182f4508ca8a81316f94b77e5e0d
Rusu Vlad <102832029+Heiwa03@users.noreply.github.com>
2025-09-11 16:20:03 +0300

    Add Seeder class for initial db testing and add gitignore (#4)
    * Created DAL and BaseId Entity
    
    * Create User and UserSession entities
    
    spizidili with modifications
    
    * Add IBaserepository interface
    
    * Add base and user repositories and interfaces and dbcontext
    
    * Create TestDataSeeder for db testing.
    
    * Add gitignore file
    
    * Nvm now gitignore should be added
    
    * Now surely gitignore should be working

---
+++ DataAccessLayer/Seeders/TestDataSeeder.cs
@@ -0,0 +1,70 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Security.Cryptography;
+
+namespace DataAccessLayer.Seeders
+{
+    public class TestDataSeeder
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public TestDataSeeder (DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+        public async Task Initialize()
+        {
+            await using var transaction = await _databaseContext.Database.BeginTransactionAsync();
+            try
+            {
+                await Seed();
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task Seed()
+        {
+            if (_databaseContext.Users.Any())
+            {
+                System.Console.WriteLine("DB already has data. No seeding.");
+                return;
+            }
+
+            var usersToAdd = new List<User>
+            {
+                new User 
+                { 
+                    Uuid = Guid.NewGuid(),
+                    Username = "Ivan",
+                    Password = "12341234"
+                },
+                new User
+                {
+                    Uuid = Guid.NewGuid(),
+                    Username = "Dan",
+                    Password = "123321123"
+                },
+                new User
+                {
+                    Uuid = Guid.NewGuid(),
+                    Username = "Vlad",
+                    Password = "12344321"
+                }
+            };
+
+            _databaseContext.Users.AddRange(usersToAdd);
+
+            await _databaseContext.SaveChangesAsync();
+        }
+    }
+}
