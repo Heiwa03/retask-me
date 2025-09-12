6017f7b3451f3ae31c8f3395cf22a04bd834fc83
BagrinD <bagrin.danu@gmail.com>
2025-09-12 15:39:26 +0300

    Was added DB for test. TODO: Test Reg in DB

--- BusinessLogicLayer/testsBagrin/Interfaces/IUserRepository1.cs
+++ BusinessLogicLayer/testsBagrin/Interfaces/IUserRepository1.cs
@@ -2,7 +2,7 @@
 using BusinessLogicLayer.testsBagrin.Entity;
 
 namespace BusinessLogicLayer.testsBagrin.Interfaces{
-    public interface IUserRepository{
+    public interface IUserRepository1{
         Task AddUser(TestUser user);
         List<TestUser> GetAllUsers();
         List<TestUser> GetAllMails();
