61616bd388b00375b75b0c21aaa3ef68cfdf1bf8
BagrinD <bagrin.danu@gmail.com>
2025-09-12 13:05:11 +0300

    Was added DAL

---
+++ DataAccessLayer/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Entities;
+
+[Index(nameof(Username), IsUnique = true)]
+public class User : BaseId
+{
+    [StringLength(255)] public required string Username { get; set; }
+    [StringLength(255)] public required string NormalizedUsername { get; set; }
+    [StringLength(255)] public required string Password { get; set; }
+    //public required DateTime RegistrationDate { get; set; }
+}
---
+++ DataAccessLayer/Entities/UserSession.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    public class UserSession : BaseId
+    {
+        public long UserId { get; set; }
+
+        [StringLength(250)] public string? RefreshToken { get; set; }
+
+        [StringLength(36)] public string? JwtId { get; set; }
+
+        public DateTime RefreshTokenExpiration { get; set; }
+
+        // This marks if the refresh token has already been used once. 
+        public bool Redeemed { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public required User User { get; set; }
+    }
+}
