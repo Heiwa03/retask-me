3053b56007342fcfe0bfc2a4dc823dbdac25a1e6
Rusu Vlad <102832029+Heiwa03@users.noreply.github.com>
2025-09-12 13:38:10 +0300

    Add DAL to regist+helper for testing  (#5)
    * Created DAL and BaseId Entity
    
    * Create User and UserSession entities
    
    spizidili with modifications
    
    * Add IBaserepository interface
    
    * Add base and user repositories and interfaces and dbcontext
    
    * Implement data access layer for basic user and user session database operations for authentication, authorization and registration. (#1)
    
    * Created DAL and BaseId Entity
    
    * Create User and UserSession entities
    
    spizidili with modifications
    
    * Add IBaserepository interface
    
    * Add base and user repositories and interfaces and dbcontext
    
    * Create TestDataSeeder for db testing.
    
    * Add gitignore file
    
    * Nvm now gitignore should be added
    
    * Now surely gitignore should be working
    
    * Change find user by username to find user by normalized username

---
+++ DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public partial class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserSession> UserSessions { get; set; }
+    }
+}
