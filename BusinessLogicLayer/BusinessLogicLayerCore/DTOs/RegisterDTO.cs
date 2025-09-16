35b2639c6e34ca42a6cf917b0dfd775ea67c30f3
BagrinD <bagrin.danu@gmail.com>
2025-09-16 14:49:59 +0300

    Remove ignored files and ghost files from current branch

---
+++ BusinessLogicLayer/BusinessLogicLayerCore/DTOs/RegisterDTO.cs
@@ -0,0 +1,24 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using BusinessLogicLayer.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace BusinessLogicLayer.DTOs{
+    public class RegisterDTO{
+
+        [Required(ErrorMessage = "Mail is required")]
+        [EmailAddress(ErrorMessage = "Invalid mail format")]
+        public string Mail { get; set; } = null!; 
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters length")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Rep password is required")]
+        public string RepeatPassword { get; set; } = null!;
+
+        public UserRole Role {get; set; } = UserRole.Client;
+    }
+}
---
+++ BusinessLogicLayer/BusinessLogicLayerCore/Services/TemporaryLoginChecker.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogicLayer.Services.Interfaces;
+
+// PLACEHOLDER TEMPORARY LOGIN CHECKER
+// MUST BE REMOVED ONCE DB IS READY
+// WARNING NOT FOR SENSIBLE PEOPLE. O_O !!!HARD CODED VALUES!!! O_O
+
+namespace BusinessLogicLayer.Services
+{
+    public class TemporaryLoginChecker : ILoginChecker
+    {
+        private const string _validUsername = "testuser";
+        private const string _validPassword = "password123";
+
+        public bool CheckCredentials(string username, string password)
+        {
+            // The logic to check credentials is now here.
+            return username == _validUsername && password == _validPassword;
+        }
+    }
+}
