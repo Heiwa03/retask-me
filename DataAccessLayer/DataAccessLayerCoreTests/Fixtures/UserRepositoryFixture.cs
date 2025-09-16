f9dc62e3d8607a4e3728de5ec8ebfbec6c6820d7
Rusu Vlad <vlad.rusu@isa.utm.md>
2025-09-16 16:18:29 +0300

    Add Fixture for testing UserRepository

---
+++ DataAccessLayer/DataAccessLayerCoreTests/Fixtures/UserRepositoryFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayerCore;
+using DataAccessLayerCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace DataAccessLayerCoreTests.Fixtures
+{
+    public class UserRepositoryFixture : IDisposable
+    {
+        public Mock<DbSet<User>> MockSet { get; private set; }
+        public Mock<DatabaseContext> MockContext { get; private set; }
+
+        public UserRepositoryFixture()
+        {
+            MockSet = new Mock<DbSet<User>>();
+            MockContext = new Mock<DatabaseContext>();
+
+            MockContext.Setup(m => m.Users).Returns(MockSet.Object);
+        }
+
+        public void Dispose() { }
+    }
+}
