a60786568ff162c8de5483ca0bdca59ce1c7a1a2
BagrinD <bagrin.danu@gmail.com>
2025-09-17 12:18:38 +0300

    Adjusted Program.cs little bit, added MockDbSet for Unit tests

---
+++ BusinessLogicLayer/BusinessLogicLayerCoreTests/MockDbSet/MockAsyncEnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace DataAccessCore.Test.MockDbSet
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading;
+
+    public class MockAsyncEnumerableExtensions<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public MockAsyncEnumerableExtensions(IEnumerable<T> enumerable)
+            : base(enumerable)
+        {
+        }
+
+        public MockAsyncEnumerableExtensions(Expression expression)
+            : base(expression)
+        {
+        }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+            => new MockAsyncEnumeratorExtensions<T>(this.AsEnumerable().GetEnumerator());
+
+        public IAsyncEnumerator<T> GetEnumerator()
+            => new MockAsyncEnumeratorExtensions<T>(this.AsEnumerable().GetEnumerator());
+
+#pragma warning disable SA1201 // Elements must appear in the correct order
+        IQueryProvider IQueryable.Provider
+#pragma warning restore SA1201 // Elements must appear in the correct order
+            => new MockAsyncDbSetExtensions<T>(this);
+    }
+}
---
+++ BusinessLogicLayer/BusinessLogicLayerCoreTests/TestRegister/CheckUniqueMail_Test.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace BusinessLogicLayerCoreTests.TestRegister{
+    public class CheckUniqueMail_Test{
+        [Fact]
+        public void CheckUniqueMail_Success(){
+            
+        }
+
+        [Fact]
+        public void CheckUniqueMail_Fail(){
+            
+        }
+    }
+}
