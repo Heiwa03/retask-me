cda9955e954a90c4d9964f45953984b52d504ed4
BagrinD <bagrin.danu@gmail.com>
2025-09-23 15:37:47 +0300

    Some changes in db

--- ReTaskMe/Migrations/20250923123403_SyncWithDatabase3.Designer.cs
+++ ReTaskMe/Migrations/20250923123403_SyncWithDatabase3.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayerCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ReTaskMe.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250923123403_SyncWithDatabase3")]
+    partial class SyncWithDatabase3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
