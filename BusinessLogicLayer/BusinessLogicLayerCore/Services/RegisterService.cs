6b4a220932e2b3776f1811063a6b5fe9b7dede08
CertanGhenadie <ghenadie.certan@isa.utm.md>
2025-09-25 18:33:10 +0300

    Register Service

--- BusinessLogicLayer/BusinessLogicLayerCore/Services/RegisterService.cs
+++ BusinessLogicLayer/BusinessLogicLayerCore/Services/RegisterService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using BusinessLogicLayerCore.Services.Interfaces;
 using BusinessLogicLayerCore.DTOs;
 using DataAccessLayerCore.Entities;
