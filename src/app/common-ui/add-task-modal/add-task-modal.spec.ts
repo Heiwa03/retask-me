5b9697e4a2b5568ad63feb40d600ddd1ba0f2e99
UP <uzun.paula777@gmail.com>
2025-09-25 19:35:38 +0300

    merge branch front-revenko in front-up and fix conflicts

--- src/app/common-ui/add-task-modal/add-task-modal.spec.ts
+++ src/app/common-ui/add-task-modal/add-task-modal.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddTaskModal } from './add-task-modal';
+import AddTaskModal from './add-task-modal';
 
 describe('AddTaskModal', () => {
   let component: AddTaskModal;
